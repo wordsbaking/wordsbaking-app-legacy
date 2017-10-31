60638802eaeae4e616d239ef4a0a6b56e17aa799
maolion <maolion.j@gmail.com>
2017-10-31 22:23:56 +0800

    Add web animations polyfills

--- src/app/app-router.animations.ts
+++ src/app/app-router.animations.ts
@@ -17,7 +17,7 @@ const q = (
 
 export const routerTransition = trigger('routerTransition', [
   transition('* <=> *', [
-    q(':enter', style({position: 'fixed', right: 0, bottom: 0, top: 0, left: 0})),
+    q(':enter', style({position: 'fixed', right: 0, bottom: 0, top: 0, left: 0, zIndex: -1})),
     sequence([q(':leave', animateChild()), q(':enter', animateChild())]),
   ]),
 ]);
