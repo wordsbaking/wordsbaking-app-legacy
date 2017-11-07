d201740ad81b5574ef3ead074c413def345355a9
maolion <maolion.j@gmail.com>
2017-11-07 16:40:07 +0800

    Path select component

---
+++ src/app/ui/select-item/select-item.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'wb-select-item',
+  templateUrl: './select-item.component.html',
+  styleUrls: ['./select-item.component.less'],
+})
+export class SelectItemComponent {}
---
+++ src/app/ui/select/select.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'wb-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.less'],
+})
+export class SelectComponent {}
