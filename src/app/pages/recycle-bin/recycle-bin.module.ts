15d65af91e5ec77cf213726383be048736f5e0a7
maolion <maolion.j@gmail.com>
2017-11-22 18:45:02 +0800

    Clean up

--- src/app/pages/recycle-bin/recycle-bin.module.ts
+++ src/app/pages/recycle-bin/recycle-bin.module.ts
@@ -2,16 +2,11 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-
 import {UIModule} from 'app/ui';
 
 import {CoreUIModule} from 'app/core/ui';
 
-import {
-  WordStack,
-  WordStackComponent,
-} from './components/word-stack/word-stack.component';
+import {WordStackComponent} from './components/word-stack/word-stack.component';
 
 import {RecycleBinView} from './recycle-bin.view';
 
@@ -35,22 +30,4 @@ const routes: Routes = [
   ],
   declarations: [RecycleBinView, WordStackComponent],
 })
-export class RecycleBinModule {
-  recentWordStack$ = new BehaviorSubject<WordStack | undefined>(undefined);
-
-  constructor() {
-    this.recentWordStack$.next({
-      title: '最近删除的词条',
-      words: [
-        'amount',
-        'whenever',
-        'Sweden',
-        'tennis',
-        'gradually',
-        'run away',
-        'safari',
-        'house',
-      ],
-    });
-  }
-}
+export class RecycleBinModule {}
